a44526d63a8bc89cbc32fcad5d7d6315685817bf
NgocHa98 <44915192+NgocHa98@users.noreply.github.com>
2018-12-26 22:51:34 +0700

    Update km2.php

--- Source/km2.php
+++ Source/km2.php
@@ -3,33 +3,30 @@
 ?>
 <br>
 <div id="phim" style="background-color: whitesmoke">
+    <?php
+        include("lib/connection.php");                
+        $sql = "select * from khuyenmai where idKM='2' ";                
+        $result=mysqli_query($conn,$sql) or die(mysql_error());
+        while($row=mysqli_fetch_array($result))
+    {?>   
     <div class="row">
         <div class="col-md-1"></div>
         <div class="col-md-10">
 
-        <br> <br><span style="font-size: 15px">
-        <font face="Comic sans MS" size="7" >Đón Giáng Sinh – Nhận Quà Xinh</font> 
-            <p> <br>Những ngày tháng 12 bận rộn, các Stars đừng quên ghé rạp để thưởng thức loạt bom tấn trong và ngoài nước
-             dịp cuối năm và nhận ngay một phần quà đủ-dùng-cả-2019 từ HM Cinema nhé.<br><br>
-
-            Từ 01.12 – 31.12.2018, các Stars sẽ có cơ hội nhận được một quyển lịch để bàn xinh xắn với thiết kế độc đáo<
-             và ấn tượng khi mua từ 02 vé xem phim + combo 2/combo 2 extra/combo 3/combo 3 extra/combo phim.<br><br>
-            Điều kiện chương trình:<br><br>
-
-            Áp dụng cho khách hàng có thẻ thành viên HM Cinema.<br>
-            Không áp dụng cho giao dịch sử dụng movie voucher.<br>
-            Không áp dụng song song với các chương trình khuyến mãi khác.<br>
-            Chương trình có thể kết thúc sớm nếu số lượng quà tặng được phát hết.<br>
-            Trong mọi trường hợp, quyết định của HM Cinema là quyết định cuối cùng.<br>
-            </p></span>
-        </div>
-        
+        <br> <br>
+        <font face="Comic sans MS" size="7" ><?php echo $row["TenKM"]?></font> 
+        <span style="font-size: 15px">
+            <p> <br> <?php echo $row["ChitietKM"]?> 
+            </p>
+</div>
 <div class="col-md-12">  
         <br> <br>
-    <center> <img src= "./style/img/km11.jpg" class="img-thumbnail" alt="">
-</center>
+<center><img src= "./style/img/<?php echo $row["hinhanh"]?>" class="img-thumbnail" alt="">
+
 </div>
 </div>
+
 <?php
+    }
     include("./includes/footer.php");
 ?>
